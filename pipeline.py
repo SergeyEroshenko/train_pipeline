5659f62376bde64472f6b6534114643211151e63
Sergey Eroshenko <sergey.eroshenko.nsk@gmail.com>
2021-05-15 00:24:36 +0300

    add optimizer

--- pipeline.py
+++ pipeline.py
@@ -3,10 +3,15 @@ from mltrading.utils import Reader, Representation, Slicer
 from sklearn.model_selection import cross_val_score ,TimeSeriesSplit, train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import precision_score, f1_score, classification_report
+from skopt.space import Real, Integer
+from skopt.utils import use_named_args
+from skopt import gp_minimize
 import warnings
 
 
 warnings.filterwarnings('ignore')
+data_path = "/Users/sergeyerosenko/cryptoex/data"
+
 multiply = 1e6
 point = 1e-2
 window_size = 10 * multiply
@@ -14,11 +19,17 @@ step = 2 * multiply
 take_profit = 10000 * point
 stop_loss = 10000 * point
 label_windows_size = window_size
+
+space = [
+    Integer(1, 7, name='max_depth'),
+    Integer(5, 5000, name='n_estimators'),
+    Real(10**-5, 10**5, 'log-uniform', name='learning_rate'),
+    Integer(2, 6, name='min_samples_split'),
+    Integer(1, 6, name='min_samples_leaf')
+]
+
 model = GradientBoostingClassifier
 cv = TimeSeriesSplit(n_splits=5)
-
-data_path = "/Users/sergeyerosenko/cryptoex/data"
-
 reader = Reader(data_path, "BTCUSD")
 slicer = Slicer("money", window_size, step, take_profit, stop_loss, label_windows_size)
 stat_repr = Representation()
@@ -39,31 +50,47 @@ X = X[: -num_intersect]
 y = y[: -num_intersect]
 print(f"Dataset parameters: num windows {X.shape[0]}, num features {X.shape[1]}.")
 
-precision_buy = np.empty(0)
-precision_sell = np.empty(0)
+@use_named_args(space)
+def objective(**params):
+    print(params)
+    precision_buy = np.empty(0)
+    precision_sell = np.empty(0)
 
-for idx, (train_idx, val_idx) in enumerate(cv.split(X)):
-    X_train, X_val = X[train_idx], X[val_idx]
-    y_train, y_val = y[train_idx], y[val_idx]
+    for idx, (train_idx, val_idx) in enumerate(cv.split(X)):
+        X_train, X_val = X[train_idx], X[val_idx]
+        y_train, y_val = y[train_idx], y[val_idx]
 
-    X_train = X_train[: -num_intersect]
-    y_train = y_train[: -num_intersect]
+        X_train = X_train[: -num_intersect]
+        y_train = y_train[: -num_intersect]
 
-    clf = model(random_state=1)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_val)
-    score = f1_score(y_val, y_pred, average='micro')
-    prec_buy = precision_score(y_val, y_pred, labels=[1], average="micro")
-    prec_sell = precision_score(y_val, y_pred, labels=[2], average="micro")
+        clf = model(random_state=1, **params)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_val)
+        val_score = f1_score(y_val, y_pred, average='weighted')
+        prec_buy = precision_score(y_val, y_pred, labels=[1], average="micro")
+        prec_sell = precision_score(y_val, y_pred, labels=[2], average="micro")
 
-    precision_buy = np.append(precision_buy, prec_buy)
-    precision_sell = np.append(precision_sell, prec_sell)
-    print(f"Val score{idx+1} = {score:.5f}")
-    print("\n", classification_report(y_val, y_pred), "\n")
+        precision_buy = np.append(precision_buy, prec_buy)
+        precision_sell = np.append(precision_sell, prec_sell)
+        print(f"Val score{idx+1} = {val_score:.5f}")
+        print("\n", classification_report(y_val, y_pred), "\n")
 
-print("Mean", precision_buy.mean(), precision_sell.mean())
-print("Std", precision_buy.std(), precision_sell.std())
+    x1 = precision_buy.mean()
+    x2 = precision_sell.mean()
+    x3 = precision_buy.std()
+    x4 = precision_sell.std()
 
+    total_score = 4 / (1 / x1 + 1 / x2 + 1 / (1 - x3) + 1 / (1 - x4))
+    print(x1, x2, x3, x4)
+    print(-total_score)
+    return -total_score
+
+result = gp_minimize(objective, space, n_calls=20, random_state=1, verbose=True)
+print('Best Score: %.3f' % (result.fun))
+print('Best Parameters: %s' % (result.x))
+exit()   
+# Testing
+clf = model(random_state=1)
 clf.fit(X, y)
 y_pred = clf.predict(X_test)
 score = classification_report(y_test, y_pred)
